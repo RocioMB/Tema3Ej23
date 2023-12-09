d5ac68c9d93f9f89050a0e03e7218e3ba485553d
Salvador P <salvadorperezd@gmail.com>
2023-12-09 18:53:24 +0100

    Agregada la solucion

---
+++ Ejercicio 23 Tema 3/Ejercicio 23 Tema 3/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Ejercicio_23_Tema_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pesosBtn = new System.Windows.Forms.Button();
+            this.cuantosAlumnosLabel = new System.Windows.Forms.Label();
+            this.porcentajeLabel = new System.Windows.Forms.Label();
+            this.pesoMedioLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pesosBtn
+            // 
+            this.pesosBtn.BackColor = System.Drawing.Color.GhostWhite;
+            this.pesosBtn.Font = new System.Drawing.Font("Ebrima", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pesosBtn.Location = new System.Drawing.Point(91, 159);
+            this.pesosBtn.Name = "pesosBtn";
+            this.pesosBtn.Size = new System.Drawing.Size(179, 83);
+            this.pesosBtn.TabIndex = 0;
+            this.pesosBtn.Text = "Introducir pesos";
+            this.pesosBtn.UseVisualStyleBackColor = false;
+            this.pesosBtn.Click += new System.EventHandler(this.pesosBtn_Click);
+            // 
+            // cuantosAlumnosLabel
+            // 
+            this.cuantosAlumnosLabel.AutoSize = true;
+            this.cuantosAlumnosLabel.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cuantosAlumnosLabel.Location = new System.Drawing.Point(347, 57);
+            this.cuantosAlumnosLabel.Name = "cuantosAlumnosLabel";
+            this.cuantosAlumnosLabel.Size = new System.Drawing.Size(0, 21);
+            this.cuantosAlumnosLabel.TabIndex = 1;
+            // 
+            // porcentajeLabel
+            // 
+            this.porcentajeLabel.AutoSize = true;
+            this.porcentajeLabel.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.porcentajeLabel.Location = new System.Drawing.Point(347, 202);
+            this.porcentajeLabel.Name = "porcentajeLabel";
+            this.porcentajeLabel.Size = new System.Drawing.Size(0, 21);
+            this.porcentajeLabel.TabIndex = 2;
+            // 
+            // pesoMedioLabel
+            // 
+            this.pesoMedioLabel.AutoSize = true;
+            this.pesoMedioLabel.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.pesoMedioLabel.Location = new System.Drawing.Point(347, 346);
+            this.pesoMedioLabel.Name = "pesoMedioLabel";
+            this.pesoMedioLabel.Size = new System.Drawing.Size(0, 21);
+            this.pesoMedioLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(1258, 544);
+            this.Controls.Add(this.pesoMedioLabel);
+            this.Controls.Add(this.porcentajeLabel);
+            this.Controls.Add(this.cuantosAlumnosLabel);
+            this.Controls.Add(this.pesosBtn);
+            this.Name = "Form1";
+            this.Text = "Ejercicio 23 Tema 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button pesosBtn;
+        private Label cuantosAlumnosLabel;
+        private Label porcentajeLabel;
+        private Label pesoMedioLabel;
+    }
+}
---
+++ Ejercicio 23 Tema 3/Ejercicio 23 Tema 3/Form1.cs
@@ -0,0 +1,90 @@
+using Microsoft.VisualBasic;
+using System;
+using System.Windows.Forms;
+
+namespace Ejercicio_23_Tema_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pesosBtn_Click(object sender, EventArgs e)
+        {
+            int peso;
+            int personas_menor_50 = 0;
+            int personas_entre_50_65 = 0;
+            int personas_entre_65_80 = 0;
+            int personas_mayor_80 = 0;
+            int total_personas = 0;
+            int suma_total_pesos = 0;
+            int porcentaje_personas_menor_50 = 0;
+            int porcentaje_personas_entre_50_65 = 0;
+            int porcentaje_personas_entre_65_80 = 0;
+            int porcentaje_personas_mayor_80 = 0;
+            int peso_medio = 0;
+
+            try
+            {
+                peso = int.Parse(Interaction.InputBox("Introduzca un peso:", "Ejercicio 23"));
+
+                if (peso >= 0)
+                {
+                    while (peso >= 0)
+                    {
+                        if (peso <= 50)
+                        {
+                            personas_menor_50++;
+                        }
+                        if (peso > 50 && peso <= 65)
+                        {
+                            personas_entre_50_65++;
+                        }
+                        if (peso > 65 && peso <= 80)
+                        {
+                            personas_entre_65_80++;
+                        }
+                        if (peso > 80)
+                        {
+                            personas_mayor_80++;
+                        }
+
+                        suma_total_pesos += peso;
+
+                        peso = int.Parse(Interaction.InputBox("Introduzca un peso:", "Ejercicio 23"));
+                    }
+                    total_personas = personas_menor_50 + personas_entre_50_65 + personas_entre_65_80 + personas_mayor_80;
+
+                    porcentaje_personas_menor_50 = (personas_menor_50 * 100) / total_personas;
+                    porcentaje_personas_entre_50_65 = (personas_entre_50_65 * 100) / total_personas;
+                    porcentaje_personas_entre_65_80 = (personas_entre_65_80 * 100) / total_personas;
+                    porcentaje_personas_mayor_80 = (personas_mayor_80 * 100) / total_personas;
+
+                    peso_medio = suma_total_pesos / total_personas;
+
+                    cuantosAlumnosLabel.Text = "Alumnos con peso menor de 50kg: " + personas_menor_50.ToString() + "\n";
+                    cuantosAlumnosLabel.Text = cuantosAlumnosLabel.Text + "Alumnos con peso mayor de 50kg y menor o igual de 65kg: " + personas_entre_50_65.ToString() + "\n";
+                    cuantosAlumnosLabel.Text = cuantosAlumnosLabel.Text + "Alumnos con peso mayor de 65 y menor o igual de 80kg: " + personas_entre_65_80.ToString() + "\n";
+                    cuantosAlumnosLabel.Text = cuantosAlumnosLabel.Text + "Alumnos con peso mayor de 80kg: " + personas_mayor_80.ToString() + "\n";
+
+                    porcentajeLabel.Text = "Los alumnos con peso menor de 50kg representan el " + porcentaje_personas_menor_50.ToString() + "% del total. \n";
+                    porcentajeLabel.Text = porcentajeLabel.Text + "Los alumnos con peso mayor de 50kg y menor o igual de 65kg representa el " + porcentaje_personas_entre_50_65.ToString() + "% del total. \n";
+                    porcentajeLabel.Text = porcentajeLabel.Text + "Los alumnos con peso mayor de 65kg y menor o igual de 80kg representa el " + porcentaje_personas_entre_65_80.ToString() + "% del total. \n";
+                    porcentajeLabel.Text = porcentajeLabel.Text + "Los alumnos con peso mayor de 80kg representan el " + porcentaje_personas_mayor_80.ToString() + "% del total. \n";
+
+                    pesoMedioLabel.Text = "El peso medio de toda la clase es " + peso_medio.ToString() + " kilos.";
+                }
+                else
+                {
+                    MessageBox.Show("Ha introducido un peso negativo.");
+                }
+            }
+            catch (FormatException fEx)
+            {
+                MessageBox.Show("Se ha producido el error: " + fEx.Message);
+            }
+        }
+    }
+}
